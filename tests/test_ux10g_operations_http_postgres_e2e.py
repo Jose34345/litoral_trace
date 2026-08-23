17fbf60a67289ee7413c886558d4898a4ffd19a9
Jose David <30633730+Jose34345@users.noreply.github.com>
2026-08-22 22:40:51 -0300

    test: advance UX10-G HTTP contract to P1-B head

--- tests/test_ux10g_operations_http_postgres_e2e.py
+++ tests/test_ux10g_operations_http_postgres_e2e.py
@@ -129,7 +129,7 @@ def test_operations_receipt_real_http_login_csrf_rls_and_posting(
     try:
         with owner_engine.begin() as connection:
             revision = connection.execute(text("SELECT version_num FROM alembic_version")).scalar_one()
-            assert revision == "022_add_integration_history"
+            assert revision == "023_add_shipment_export_cases"
 
             organization_id = int(
                 connection.execute(
