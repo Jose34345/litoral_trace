608cb514995f769b685407a1d2c8fc68873b4545
Jose David <30633730+Jose34345@users.noreply.github.com>
2026-08-27 16:43:54 -0300

    feat(assurance): initialize Assurance domain package

---
+++ src/litoral_trace/assurance/__init__.py
@@ -0,0 +1,19 @@
+"""Assurance v1 domain package."""
+
+from litoral_trace.assurance.domain import (
+    ConfidenceLevel,
+    DocumentProcessingStatus,
+    DocumentType,
+    ExtractionRunStatus,
+    LinkedEntityType,
+    LinkMethod,
+)
+
+__all__ = [
+    "ConfidenceLevel",
+    "DocumentProcessingStatus",
+    "DocumentType",
+    "ExtractionRunStatus",
+    "LinkedEntityType",
+    "LinkMethod",
+]
